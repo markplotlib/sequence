429a9fdb01f83a4467a61641f0e377b913d049c3
Mark Chesney <mchesney@seattleu.edu>
2019-11-23 13:24:07 -0800

    add welcome, goodbye, and user loop to driver

--- hw7.cpp
+++ hw7.cpp
@@ -5,6 +5,22 @@
 using namespace std;
 
 int main() {
-    cout << "TEST" << endl;
+    int runNum = 1;
+    char runNewWord = 'y';
+    char runAgain = 'y';
+        
+    cout << "\nWelcome to the Sequence tester!\n" << endl;
+    cout << "--------------" << endl;
+
+    while (runAgain == runNewWord) {
+        cout << "Testing word " << runNum << endl;
+        cout << "Would you like to test another word (y/n)? ";
+        cin >> runAgain;
+        runNum++;
+    }
+
+    cout << "\n--------------" << endl;
+    cout << "Thank you for using the Sequence tester!\n" << endl;
     return 0;
 }
+
