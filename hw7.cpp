811fd8c984dec7b689c87e82aa2dcd2f77bead5b
Mark Chesney <mchesney@seattleu.edu>
2019-11-24 18:44:57 -0800

    simplify driver file

--- hw7.cpp
+++ hw7.cpp
@@ -8,21 +8,13 @@ using namespace std;
 int main() {
     string WORD_FILE = "words.dat";
     ifstream infile;
-    int runNum = 1;
-    const int RUN_MIN = 4;
-    const char REPEAT = 'y';
-    char runAgain = 'y';
+    int NUM_WORDS = 48;
+    string * wordArr = new string[NUM_WORDS];
         
-string tempAns = "ADULT";
-string tempHintEmit = "ADULLT";
-string tempHintInvert = "AUDLT";
-string guess;
-
     cout << "\nWelcome to the Sequence tester!\n" << endl;
     cout << "--------------" << endl;
 
-    int NUM_WORDS = 48;
-    string * wordArr = new string[NUM_WORDS];
+    // read in words file
     infile.open(WORD_FILE);
     int i = 0;
     for (string word; getline(infile, word); ) {
@@ -31,30 +23,8 @@ string guess;
     infile.close();
 
     for (int j = 0; j < NUM_WORDS; j++) {
-        cout << wordArr[j] << " ";
+        cout << wordArr[j] << " " << rand() % 48 + 1 << " ";
     }
-
-    // while (runAgain == REPEAT) {
-/*
-        cout << "Testing word " << runNum << "\n" << endl;
-        cout << "emit: " << tempHintEmit << endl;
-        cout << "invert: " << tempHintInvert << "\n" << endl;
-        cout << "What is your guess? ";
-        cin >> guess;
-        while (runAgain == REPEAT && guess != tempAns) {
-            cout << "That is not correct. \nGuess again (y/n)? ";
-            cin >> runAgain;
-        }
-        cout << "That's correct!\n";
-        cout << "\n--------------" << endl;
-        if (runNum > RUN_MIN) {
-            cout << "Would you like to test another word (y/n)? ";
-            cin >> runAgain;
-        }
-        runNum++;
-*/
-    // }
-cout << REPEAT << RUN_MIN << runAgain << runNum << endl;
 
     cout << "\n--------------" << endl;
     cout << "Thank you for using the Sequence tester!\n" << endl;
