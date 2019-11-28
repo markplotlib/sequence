734d24124951c04e00a80dfa5d96128a9deef105
Mark Chesney <mchesney@seattleu.edu>
2019-11-27 21:22:31 -0800

    fix error in Repeater.cpp

--- hw7.cpp
+++ hw7.cpp
@@ -1,14 +1,11 @@
 // Driver file
-#include "Sequence.h"
+#include "Repeater.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Repeater * rep = new Repeater();
-    // cout << "Active State? " << boolalpha << rep->isActive() << endl;
-
-    Sequence * seq = new Sequence();
-    cout << "Active State? " << boolalpha << seq->isActive() << endl;
+    Repeater * rep = new Repeater();
+    cout << "Active State? " << boolalpha << rep->isActive() << endl;
 
     // cout << "Making a guess, that word is FAT: " << boolalpha << seq->guess("FAT") << endl;
     // cout << "Making a guess, that word is CAT: " << boolalpha << seq->guess("CAT") << endl;
